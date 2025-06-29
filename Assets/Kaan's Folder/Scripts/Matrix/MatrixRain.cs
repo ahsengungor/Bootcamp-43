0b2c7d01c49c80bbdafe8bd88d187fbc9710c59a
Kaan Avdan <kaanavdan01@gmail.com>
2025-06-29 18:56:00 +0300

    Hardness for QTE Puzzle

--- Assets/Kaan's Folder/Scripts/Matrix/MatrixRain.cs
+++ Assets/Kaan's Folder/Scripts/Matrix/MatrixRain.cs
@@ -53,12 +53,6 @@ public class MatrixRain : MonoBehaviour
         StartCoroutine(AnimateColumn(newCol));
     }
 
-    private void Update()
-    {
-        Debug.Log($"Panel Rect Yukseklik : {spawnArea.rect.height} " +
-            $"\nAnchor Pozisyonlarý : {spawnArea.anchoredPosition.y}");
-    }
-
     IEnumerator AnimateColumn(GameObject column)
     {
         RectTransform rect = column.GetComponent<RectTransform>();
@@ -96,7 +90,6 @@ public class MatrixRain : MonoBehaviour
 
             yield return null;
         }
-            Debug.Log(column.gameObject.name + "Spawned at " + rect.anchoredPosition);
 
         Destroy(column);
     }
