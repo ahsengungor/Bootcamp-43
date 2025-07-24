8ef2efc428750907f1d94368afb69903d2d0f038
Ahsen Büşra Güngör <bgngr14@gmail.com>
2025-07-24 21:21:48 +0300

    main ORGANIZATION DELETE

--- Assets/Kaan's Folder/Scripts/Player/Interaction/IInteractable.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public interface IInteractable
-{
-    string GetInteractionPrompt();
-    void Interact();
-    Transform Transform { get; }
-}
--- Assets/Kaan's Folder/Scripts/State Machine/INPCState.cs
+++
@@ -1,6 +0,0 @@
-public interface INPCState
-{
-    void Enter();
-    void Update();
-    void Exit();
-}
