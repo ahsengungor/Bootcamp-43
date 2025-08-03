66081f26abeafd131794bfa844df9b1b12fc2bab
Ahsen Büşra Güngör <bgngr14@gmail.com>
2025-08-03 19:40:47 +0300

    Delete Assets directory

--- Assets/Scripts/Matrix_Scripts/WordSettings.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class WordSettings : MonoBehaviour
-{
-    [Tooltip("Bu obje spawn edildikten sonra, sonraki spawn için kaç saniye beklenmeli.")]
-    public float waitDuration = 1.0f;
-}
